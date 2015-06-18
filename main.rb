6cc3733df09faf8a3e2b2fb56035a4f9f91ee597
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2015-06-18 14:17:41 -0400

    Prompt users for input infinitely

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require_relative("lib/game")
 
 rooms = {
-  "2,0" => "starting room",
+  "2,0" => "You are in a room full of strangers. Naked.",
   "2,1" => "room 2,1",
   "3,1" => "room 3,1",
   "4,1" => "game over",
@@ -13,4 +13,16 @@ rooms = {
 }
 
 game = Game.new(rooms, "2,0")
+
+puts ""
 puts game.current_room
+puts ""
+
+input = nil
+while input != "exit"
+  puts "What would you like to do?"
+  print "> "
+  input = gets.chomp
+  puts game.current_room
+  puts ""
+end
